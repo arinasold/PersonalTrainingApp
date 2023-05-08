c90480a8478ae4c7810a990393b19a8412644230
Arina <arina.soldan@myy.haaga-helia.fi>
2023-05-08 19:30:02 +0300

    Dropdown-menu update

--- src/App.js
+++ src/App.js
@@ -5,21 +5,23 @@ import Trainings from './components/training';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import Statistics from './components/statisticsPage';
 import CalendarForTrainings from './components/calender';
 
 function App() {
-  
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
 
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
   };
 
-  
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <div className="App">
@@ -30,58 +32,29 @@ function App() {
               edge="start"
               color="inherit"
               aria-label="menu"
-              onClick={toggleMenu}
+              onClick={handleClick}
             >
               <MenuIcon />
             </IconButton>
             <Typography variant="h6">Personal trainer app</Typography>
-            {menuOpen && (
-              <div
-                style={{
-                  position: 'absolute',
-                  backgroundColor: 'white',
-                  top: '60px',
-                  left: '0px',
-                  width: '140px',
-                  height: '250px',
-                  boxShadow: '1px 1px 5px grey',
-                  zIndex: 1,
-                }}
-              >
-                <Button
-                  component={Link}
-                  to="/customers"
-                  color="primary"
-                  style={{ margin: '10px' }}
-                >
-                  Customers
-                </Button>
-                <Button
-                  component={Link}
-                  to="/trainings"
-                  color="primary"
-                  style={{ margin: '10px' }}
-                >
-                  Trainings
-                </Button>
-                <Button
-                  component={Link}
-                  to="/calender"
-                  color="primary"
-                  style={{ margin: '10px' }}
-                >
-                  Calendar
-                </Button>
-                <Button
-                  component={Link}
-                  to="/statistics"
-                  color="primary"
-                  style={{ margin: '10px' }}
-                >
-                  Statistics
-                </Button>
-              </div>
-            )}
+            <Menu
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose} component={Link} to="/customers">
+                Customers
+              </MenuItem>
+              <MenuItem onClick={handleClose} component={Link} to="/trainings">
+                Trainings
+              </MenuItem>
+              <MenuItem onClick={handleClose} component={Link} to="/calender">
+                Calendar
+              </MenuItem>
+              <MenuItem onClick={handleClose} component={Link} to="/statistics">
+                Statistics
+              </MenuItem>
+            </Menu>
           </Toolbar>
         </AppBar>
         <Routes>
